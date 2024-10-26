d7ee5ca92ba9f9b6baf950ba62262b464468a024
yadunandan-rgb <yadunandana@vegam.co>
2024-10-26 10:03:58 +0530

    changes

--- Contest/LeetCode/con08042023/convertArrayTo2dArray.java
+++ Contest/LeetCode/con08042023/convertArrayTo2dArray.java
@@ -3,32 +3,32 @@ import java.util.*;;
 
 public class convertArrayTo2dArray {
     public List<List<Integer>> findMatrix(int[] nums) { 
-        List<List<Integer>> ans = new ArrayList(); 
-        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>(); 
+        List<List<Integer>> ansList = new ArrayList(); 
+        HashMap<Integer,Integer> mapArrItemCounter = new HashMap<Integer,Integer>(); 
 
         for(int i = 0;i< nums.length; i++){ 
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1); 
+            mapArrItemCounter.put(nums[i],mapArrItemCounter.getOrDefault(nums[i],0)+1); 
         } 
  
         int  maxRowCount = 1; 
-        for(HashMap.Entry<Integer,Integer> entry : map.entrySet()){ 
+        for(HashMap.Entry<Integer,Integer> entry : mapArrItemCounter.entrySet()){ 
             maxRowCount = Math.max(entry.getValue(), maxRowCount); 
         } 
 
         for(int i = 0; i < maxRowCount; i++){ 
-            ans.add(new ArrayList()); 
+            ansList.add(new ArrayList()); 
         } 
 
         int rowToInsert = 0; 
-        for(HashMap.Entry<Integer,Integer> entry : map.entrySet()){ 
+        for(HashMap.Entry<Integer,Integer> entry : mapArrItemCounter.entrySet()){ 
             rowToInsert=0; 
 
             while(rowToInsert < entry.getValue()){ 
-                ans.get(rowToInsert).add(entry.getKey()); 
+                ansList.get(rowToInsert).add(entry.getKey()); 
                 rowToInsert++; 
             } 
         } 
 
-        return ans; 
+        return ansList; 
     }  
 }
